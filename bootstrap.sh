3f835d97c5852801e87530b50a69911f5465f86d
Bryan Valletta <bryan.valletta@firefly.co>
2025-06-18 17:32:40 -0400

    Add support for aarch64 architecture

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ help_string="Available values are: imx8, imx7"
 
 DEST=${DEST:-$(pwd)}
 SRC=${SRC:-$(pwd)}
+ARCH_TOOLS=""
 
 if [[ -z ${SOC} ]];then
 cat << eom
@@ -23,9 +24,20 @@ eom
 exit 2
 fi
 
+# Get the architecture to use
+if [ "$(uname -m)" = "aarch64" ]; then
+echo "Running on ARM64 (aarch64) architecture"
+ARCH_TOOLS="cst-4.0.0-aarch64"
+PLATFORM_BIN="linux-arm64"
+else
+echo "Running on x86_64 architecture"
+ARCH_TOOLS="cst-4.0.0"
+PLATFORM_BIN="linux64"
+fi
+
 do_configure () {
     install -d ${DEST}/cst-tools
-    tar -C ${DEST}/cst-tools -xf ${SRC}/nxp/cst-4.0.0.tgz --strip-components=1 cst-4.0.0/linux64 cst-4.0.0/keys cst-4.0.0/crts cst-4.0.0/ca
+    tar -C ${DEST}/cst-tools -xf ${SRC}/nxp/${ARCH_TOOLS}.tgz --strip-components=1 ${ARCH_TOOLS}/${PLATFORM_BIN} ${ARCH_TOOLS}/keys ${ARCH_TOOLS}/crts ${ARCH_TOOLS}/ca
 
     for d in env Makefile hab tools;do
         cp -a ${SRC}/${SOC}/${d} ${DEST}/cst-tools/
--- imx8/tools/gen_srk.sh
+++ imx8/tools/gen_srk.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$(pwd)/../linux64/bin/srktool -h 4 -t SRK_1_2_3_4_table.bin -e SRK_1_2_3_4_fuse.bin -d sha256 -c SRK1_sha256_2048_65537_v3_ca_crt.pem,SRK2_sha256_2048_65537_v3_ca_crt.pem,SRK3_sha256_2048_65537_v3_ca_crt.pem,SRK4_sha256_2048_65537_v3_ca_crt.pem -f 1
+if [ $(uname -m) = "aarch64" ]
+then
+    bin_path="linux-arm64/bin"
+else
+    bin_path="linux64/bin"
+fi
+
+$(pwd)/../${bin_path}/srktool -h 4 -t SRK_1_2_3_4_table.bin -e SRK_1_2_3_4_fuse.bin -d sha256 -c SRK1_sha256_2048_65537_v3_ca_crt.pem,SRK2_sha256_2048_65537_v3_ca_crt.pem,SRK3_sha256_2048_65537_v3_ca_crt.pem,SRK4_sha256_2048_65537_v3_ca_crt.pem -f 1
